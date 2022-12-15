895a7d457a78c1a56f8521e19dc047b5bf51963a
Bapugouda <rajjdanush12@gmail.com>
2022-12-15 18:03:26 +0530

    First commit

---
+++ src/createDatabase.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const subscriberModel = require("./models/subscribers");
+const data = require("./data");
+
+// Connect to DATABASE
+const DATABASE_URL =
+  "mongodb+srv://bapu-gouda:902781@almabetterproject.am9fvts.mongodb.net/test";
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Database created..."));
+
+const refreshAll = async () => {
+  await subscriberModel.deleteMany({});
+  //console.log(connection)
+  await subscriberModel.insertMany(data);
+  //console.log(data)
+  await mongoose.disconnect();
+};
+refreshAll();
